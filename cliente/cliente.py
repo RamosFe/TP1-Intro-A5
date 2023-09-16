f5a47c05dd5d46a2fd647bb26c9a96a6a14c2d35
Eliana Harriet <eharriet@fi.uba.ar>
2023-09-16 16:22:30 -0300

    Parsing

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -2,38 +2,30 @@
 import argparse
 
 SERVER_PORT = 3030
-help_command_source = "help_command.txt"
-
-
-# def help_command(args):
-#     with open('help_command_source', 'r') as file:
-#         print(file.read())
-
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='<command description>', add_help=False)
+    
+    parser = argparse.ArgumentParser(description='FileTransfer')
     subparsers = parser.add_subparsers(dest='command')
 
     upload_parser = subparsers.add_parser('upload')
     upload_parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     upload_parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
-    upload_parser.add_argument('-H', '--help', action='store_true', help='show this help message and exit')
-    upload_parser.add_argument('--host', type=str, help='server IP address')
+    upload_parser.add_argument('-H','--host', type=str, help='server IP address')
     upload_parser.add_argument('-p', '--port', type=int, help='server port')
-    upload_parser.add_argument('--src', type=str, help='source file path')
+    upload_parser.add_argument('-s','--src', type=str, help='source file path')
     upload_parser.add_argument('-n', '--name', type=str, help='file name')
 
     download_parser = subparsers.add_parser('download')
     download_parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     download_parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
-    download_parser.add_argument('-H', '--help', action='store_true', help='show this help message and exit')
-    download_parser.add_argument('--host', type=str, help='server IP address')
+    download_parser.add_argument('-H','--host', type=str, help='server IP address')
     download_parser.add_argument('-p', '--port', type=int, help='server port')
-    download_parser.add_argument('--dst', type=str, help='destination file path')
+    download_parser.add_argument('-d','--dst', type=str, help='destination file path')
     download_parser.add_argument('-n', '--name', type=str, help='file name')
 
-    parser.add_argument('-h', '--help', action='help', help='show this help message and exit')
+    # TODO complete a gral help message abou the client
 
     args = parser.parse_args()
 
@@ -66,7 +58,5 @@ def main():
         # Handle download command
         print(f'Downloading {args.name} from {args.host}:{args.port} to {args.dst}')
     
-    if args.help:
-        parse_arguments().print_help()
 
 main()
