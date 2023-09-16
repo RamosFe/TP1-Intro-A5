7d2c8f069e05f3747f12f674ceca0711f3a8933a
Eliana Harriet <eharriet@fi.uba.ar>
2023-09-16 17:08:47 -0300

    Client side

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,62 +1,57 @@
+import parser as ps
 
-import argparse
+import socket
 
-SERVER_PORT = 3030
-
-
-def parse_arguments():
-    
-    parser = argparse.ArgumentParser(description='FileTransfer')
-    subparsers = parser.add_subparsers(dest='command')
-
-    upload_parser = subparsers.add_parser('upload')
-    upload_parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-    upload_parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
-    upload_parser.add_argument('-H','--host', type=str, help='server IP address')
-    upload_parser.add_argument('-p', '--port', type=int, help='server port')
-    upload_parser.add_argument('-s','--src', type=str, help='source file path')
-    upload_parser.add_argument('-n', '--name', type=str, help='file name')
-
-    download_parser = subparsers.add_parser('download')
-    download_parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-    download_parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
-    download_parser.add_argument('-H','--host', type=str, help='server IP address')
-    download_parser.add_argument('-p', '--port', type=int, help='server port')
-    download_parser.add_argument('-d','--dst', type=str, help='destination file path')
-    download_parser.add_argument('-n', '--name', type=str, help='file name')
-
-    # TODO complete a gral help message abou the client
-
-    args = parser.parse_args()
-
-    if args.help:
-        parser.print_help()
-        quit()
-
-    return args
-
-
-#########
+def verify_params(args):
+    if not args.host or not args.port:
+        return False
+    if args.command == 'upload' and (not args.src or not args.name):
+        return False
+    if args.command == 'download' and (not args.dst or not args.name):
+        return False
+    return True
 
 def main():
-    args = parse_arguments()
-    if args.command == 'upload':
-        if not args.host or not args.port or not args.src or not args.name:
-            print('Error: missing required argument(s)')
-            parse_arguments().print_help()
-            return
-
-        # Handle upload command
-        print(f'Uploading {args.src} to {args.host}:{args.port} with name {args.name}')
-        
-    elif args.command == 'download':
-        if not args.host or not args.port or not args.dst or not args.name:
-            print('Error: missing required argument(s)')
-            parse_arguments().print_help()
-            return
-
-        # Handle download command
-        print(f'Downloading {args.name} from {args.host}:{args.port} to {args.dst}')
+    args = ps.parse_arguments()
     
+    if not verify_params(args):
+        print('Error: missing required argument(s)')
+        ps.parse_arguments().print_help()
+        return
+    
+    # if args.command == 'upload':
+    #     print(f'Uploading {args.src} to {args.host}:{args.port} with name {args.name}')
+        
+    # elif args.command == 'download':
+    #     print(f'Downloading {args.name} from {args.host}:{args.port} to {args.dst}')
+
+    address = (f"{args.host}:{args.port}")
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        client_socket.connect(address)
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure the server is running.")
+        return 
+    except TimeoutError:
+        print("Connection timed out. Check the host and port.")
+        return 
+    except BrokenPipeError:
+        print("Connection closed. Reconnect and try again.")
+        return 
+    except Exception as e: # TODO specify exception and handle it
+        print(f'Error: {e}')
+        return
+
+    # send data to the server
+    message = 'Hello, server!'
+    client_socket.sendall(message.encode())
+    
+    # receive data from the server
+    data = client_socket.recv(1024)
+    print(f'Received data: {data.decode()}')
+
+    # close the socket
+    client_socket.close()
 
 main()
